d343309675e22da76f21b0b5397d9250e307433b
M0000000N <ldh1923@naver.com>
2022-09-14 20:48:19 +0900

    220914

--- Node/220914/3.5.7.js
+++ Node/220914/3.5.7.js
@@ -1,15 +1,18 @@
 const { exit } = require('process');
-const {
-    Worker, isMainThread, parentPort,
-} = require('worker_threads');
+const {Worker, isMainThread, parentPort,} 
+= require('worker_threads');
 
-if(isMainThread) {
+if(isMainThread) // 부모일때
+{
     const worker = new Worker(__filename);
     worker.on('message', message => console.log('from worker', message));
     worker.on('exit', ()=> console.log('worker exit'));
     worker.postMessage('ping');
-} else{
-    parentPort.on('message', (value) => {
+} 
+else // 워커일때
+{
+    parentPort.on('message', (value) => 
+    {
         console.log('from parent', value);
         parentPort.postMessage('pong');
         parentPort.close();
